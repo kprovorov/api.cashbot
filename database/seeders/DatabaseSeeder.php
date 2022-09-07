f11d9f7cd765212d42744efb8ae1a9b41b19c662
Kirill Provorov <kirill@provorov.dev>
2022-09-07 22:54:11 +0200

    f

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call(AccountSeeder::class);
-
         Account::factory()
                ->count(3)
                ->hasJars(1, [
