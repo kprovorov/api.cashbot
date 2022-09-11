1f5402d619a4f336c5e7670b380530795e1a3ed0
Kirill Provorov <kirill@provorov.dev>
2022-09-11 17:52:51 +0200

    refresh balances

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -5,19 +5,29 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreAccountRequest;
 use App\Http\Requests\UpdateAccountRequest;
 use App\Models\Account;
+use App\Services\AccountService;
 use DB;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Spatie\DataTransferObject\Exceptions\UnknownProperties;
 
 class AccountController extends Controller
 {
+    public function __construct(protected readonly AccountService $accountService)
+    {
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return Collection
+     * @throws UnknownProperties
      */
     public function index(): Collection
     {
+        // Refresh balances
+        $this->accountService->updateAccountBalances();
+        
         return Account::with([
             'jars',
             'payments' => function (HasManyThrough $query) {
