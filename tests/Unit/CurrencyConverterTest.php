c5151fb85812724e460de7549926fcdfba645869
Kirill Provorov <kirill@provorov.dev>
2022-09-25 14:27:39 +0200

    fix test

--- tests/Unit/CurrencyConverterTest.php
+++ tests/Unit/CurrencyConverterTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Unit;
 
 use App\Enums\Currency;
+use App\Monobank\DTO\Collections\RateDataCollection;
+use App\Monobank\DTO\RateData;
 use App\Monobank\Services\MonobankService;
 use App\Services\CurrencyConverter;
+use Spatie\DataTransferObject\Exceptions\UnknownProperties;
 use Tests\TestCase;
 
 class CurrencyConverterTest extends TestCase
@@ -12,33 +15,36 @@ class CurrencyConverterTest extends TestCase
     /**
      * @test
      * @return void
+     * @throws UnknownProperties
      */
-    public function it_successfully_converts_EUR_to_UAH_currency(): void
+    public function it_successfully_converts_eur_to_uah_currency(): void
     {
         $mock = $this->mock(MonobankService::class);
-        $mock->shouldReceive('getRates')->once()->andReturn([
-            [
-                "currencyCodeA" => 840,
-                "currencyCodeB" => 980,
-                "date"          => 1663794609,
-                "rateBuy"       => 36.65,
-                "rateSell"      => 37.9507,
-            ],
-            [
-                "currencyCodeA" => 978,
-                "currencyCodeB" => 980,
-                "date"          => 1663861209,
-                "rateBuy"       => 36.1,
-                "rateSell"      => 37.9507,
-            ],
-            [
-                "currencyCodeA" => 978,
-                "currencyCodeB" => 840,
-                "date"          => 1663861209,
-                "rateBuy"       => 0.982,
-                "rateSell"      => 1,
-            ],
-        ]);
+        $mock->shouldReceive('getRates')->once()->andReturn(
+            new RateDataCollection([
+                new RateData([
+                    "currencyCodeA" => 840,
+                    "currencyCodeB" => 980,
+                    "date"          => 1663794609,
+                    "rateBuy"       => 36.65,
+                    "rateSell"      => 37.9507,
+                ]),
+                new RateData([
+                    "currencyCodeA" => 978,
+                    "currencyCodeB" => 980,
+                    "date"          => 1663861209,
+                    "rateBuy"       => 36.1,
+                    "rateSell"      => 37.9507,
+                ]),
+                new RateData([
+                    "currencyCodeA" => 978,
+                    "currencyCodeB" => 840,
+                    "date"          => 1663861209,
+                    "rateBuy"       => 0.982,
+                    "rateSell"      => 1,
+                ]),
+            ])
+        );
         $service = $this->app->make(CurrencyConverter::class);
 
 
@@ -50,33 +56,36 @@ class CurrencyConverterTest extends TestCase
     /**
      * @test
      * @return void
+     * @throws UnknownProperties
      */
-    public function it_successfully_converts_UAH_to_EUR_currency(): void
+    public function it_successfully_converts_uah_to_eur_currency(): void
     {
         $mock = $this->mock(MonobankService::class);
-        $mock->shouldReceive('getRates')->once()->andReturn([
-            [
-                "currencyCodeA" => 840,
-                "currencyCodeB" => 980,
-                "date"          => 1663794609,
-                "rateBuy"       => 36.65,
-                "rateSell"      => 37.9507,
-            ],
-            [
-                "currencyCodeA" => 978,
-                "currencyCodeB" => 980,
-                "date"          => 1663861209,
-                "rateBuy"       => 36.1,
-                "rateSell"      => 37.9507,
-            ],
-            [
-                "currencyCodeA" => 978,
-                "currencyCodeB" => 840,
-                "date"          => 1663861209,
-                "rateBuy"       => 0.982,
-                "rateSell"      => 1,
-            ],
-        ]);
+        $mock->shouldReceive('getRates')->once()->andReturn(
+            new RateDataCollection([
+                new RateData([
+                    "currencyCodeA" => 840,
+                    "currencyCodeB" => 980,
+                    "date"          => 1663794609,
+                    "rateBuy"       => 36.65,
+                    "rateSell"      => 37.9507,
+                ]),
+                new RateData([
+                    "currencyCodeA" => 978,
+                    "currencyCodeB" => 980,
+                    "date"          => 1663861209,
+                    "rateBuy"       => 36.1,
+                    "rateSell"      => 37.9507,
+                ]),
+                new RateData([
+                    "currencyCodeA" => 978,
+                    "currencyCodeB" => 840,
+                    "date"          => 1663861209,
+                    "rateBuy"       => 0.982,
+                    "rateSell"      => 1,
+                ]),
+            ])
+        );
         $service = $this->app->make(CurrencyConverter::class);
 
 
