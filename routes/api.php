74d7155b6834b3a324750400dfb9a2c18fd2dca3
Kirill Provorov <kirill@provorov.dev>
2023-03-23 18:53:52 +0100

    sanctum token

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use App\AccountModule\Controllers\AccountController;
 use App\Http\Controllers\RatesController;
 use App\PaymentModule\Controllers\PaymentController;
+use App\UserModule\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +21,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Sanctum token
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
 });
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
